25294e28f9d9902e438914962074c74f799c6b43
RuthMery <ruth.cardenas.perez@gmail.com>
2017-03-05 15:40:50 -0500

    validando datos vacios

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,16 @@ printData.onclick = function (){
     var dni = document.getElementById('dni').value;
     var address = document.getElementById('address').value;
     var board = document.getElementById('board');
-    card = "<ul><li>Nombre: "+name+"</li>"+
-           "<li>Apellido: "+lastName+"</li>"+
-           "<li>DNI: "+dni+"</li>"+
-           "<li>Direccion: "+address+"</li>"+
-           "</ul>";
-    board.innerHTML = card;
-    cleanForm();
+    if (name == "" || lastName == "" || dni == "" || address == "") {
+      alert("Debe ingresar todos los Datos solicitados");
+    }else {
+      card = "<ul><li>Nombre: "+name+"</li>"+
+             "<li>Apellido: "+lastName+"</li>"+
+             "<li>DNI: "+dni+"</li>"+
+             "<li>Direccion: "+address+"</li>"+
+             "</ul>";
+      board.innerHTML = card;
+      cleanForm();
+    }
     //return false;
   }
